76c10fb776a599215d61438b77744493fcb8da84
bryanc <quakeaz@gmail.com>
2023-12-15 19:50:35 -0700

    Progress. Completed System update card.

--- app/src/main/java/com/bryancandi/android/updateassistant/MainFragment.java
+++ app/src/main/java/com/bryancandi/android/updateassistant/MainFragment.java
@@ -1,9 +1,12 @@
 package com.bryancandi.android.updateassistant;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -13,11 +16,18 @@ import androidx.navigation.fragment.NavHostFragment;
 
 import com.bryancandi.android.updateassistant.databinding.FragmentMainBinding;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Objects;
 
 public class MainFragment extends Fragment {
 
+    CardView versionCard;
     CardView securityCard;
+    TextView version;
+    TextView securityUpdate;
 
     private FragmentMainBinding binding;
 
@@ -34,11 +44,53 @@ public class MainFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        securityCard = view.findViewById(R.id.security_card_view);
-        securityCard.setOnClickListener(v -> {
-            Toast.makeText(requireActivity(), "Button was Clicked", Toast.LENGTH_LONG).show();
+        version = view.findViewById(R.id.versionTV);
+        version.setText(getString(R.string.android_version) + " " + androidVersion());
+        securityUpdate = view.findViewById(R.id.securityUpdateTV);
+        securityUpdate.setText(securityUpdate());
+
+        versionCard = view.findViewById(R.id.version_card_view);
+        versionCard.setOnClickListener(v -> {
+            Toast.makeText(getActivity(), R.string.android_version_toast,
+                    Toast.LENGTH_LONG).show();
         });
 
+        securityCard = view.findViewById(R.id.security_card_view);
+        securityCard.setOnClickListener(v -> {
+            try {
+                Intent intent = new Intent("com.google.android.gms.update.SystemUpdateActivity");
+                intent.setClassName("com.google.android.gms", "com.google.android.gms.update.SystemUpdateActivity");
+                startActivity(intent);
+            } catch (Exception e) {
+                e.printStackTrace();
+                Toast.makeText(getActivity(), R.string.not_available,
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+
+    private String androidVersion() {
+        return Build.VERSION.RELEASE;
+    }
+
+    private String securityUpdate() {
+        String patchDate = Build.VERSION.SECURITY_PATCH;
+        SimpleDateFormat spf = new SimpleDateFormat("yyyy-MM-dd");
+        Date newPatchDate = null;
+        try {
+            newPatchDate = spf.parse(patchDate);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.LONG);
+        String newPatchDateString;
+        if (newPatchDate != null) {
+            newPatchDateString = dateFormat.format(newPatchDate);
+            return newPatchDateString;
+        } else {
+            return Build.VERSION.SECURITY_PATCH;
+        }
     }
 
     @Override
